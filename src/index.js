37981a108e4ea48344cb6d3a149a1ac1a4db71c4
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2019-11-04 17:24:25 -0300

    converting to useReducer instead of useState

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
+const notesReducer = (state, action) => {
+  switch (action.type) {
+    case "POPULATE_NOTES":
+      return action.notes;
+    case "ADD_NOTE":
+      return [...state, action.note];
+    case "REMOVE_NOTE":
+      return state.filter(note => note.title !== action.title);
+    default:
+      return state;
+  }
+};
+
 const NoteApp = () => {
-  const [notes, setNotes] = useState([]);
+  const [notes, dispatch] = useReducer(notesReducer, []);
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
@@ -11,7 +24,7 @@ const NoteApp = () => {
     const storedNotes = JSON.parse(window.localStorage.getItem("notes"));
 
     if (storedNotes) {
-      setNotes(storedNotes);
+      dispatch({ type: "POPULATE_NOTES", notes: storedNotes });
     }
   }, []);
 
@@ -21,13 +34,13 @@ const NoteApp = () => {
 
   const addNote = event => {
     event.preventDefault();
-    setNotes([...notes, { title, body }]);
+    dispatch({ type: "ADD_NOTE", note: { title, body } });
     setTitle("");
     setBody("");
   };
 
   const removeNote = title => {
-    setNotes(notes.filter(note => note.title !== title));
+    dispatch({ type: "REMOVE_NOTE", title });
   };
 
   return (
