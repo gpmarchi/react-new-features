d35139fe670a804f498953d2ffcfd627c223b6d4
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2019-11-04 10:17:21 -0300

    add simple test component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = props => {
+  const [count, setCount] = useState(props.count);
+  const [text, setText] = useState("");
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  const printText = event => {
+    const textInput = event.target.value;
+    setText(textInput);
+  };
+
+  return (
+    <div>
+      <p>
+        the current {text || "count"} is {count}
+      </p>
+      <button onClick={increment}>+1</button>
+      <button onClick={reset}>reset</button>
+      <button onClick={decrement}>-1</button>
+      <input type="text" value={text} onChange={printText} />
+    </div>
+  );
+};
+
+App.defaultProps = {
+  count: 0
+};
+
+ReactDOM.render(<App count={10} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
